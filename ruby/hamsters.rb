5f80231ffa5386a74ab6e653676ffab8e0004552
Justincadburywong <justincadburywong@gmail.com>
2016-03-23 12:48:26 -0700

    edited hamsters.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,18 @@
+puts "whats is your name?"
+name=gets.chomp
+
+puts "what is your volume level?"
+volume=gets.chomp
+
+puts "what is your fur color?"
+color=gets.chomp
+
+puts "are you a good hamster?"
+good=gets.chomp
+
+puts "how old are you?"
+age=gets.chomp
+
+if age="",
+	age=nil
+end
