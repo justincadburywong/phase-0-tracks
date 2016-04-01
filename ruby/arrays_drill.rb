0bc54112d9676be9d0ee3bd70f82164de4da862a
Justincadburywong <justincadburywong@gmail.com>
2016-03-31 20:19:03 -0700

    release 1 done

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,1 +1,26 @@
- 
+California = []
+p California
+California << "San Diego"
+California << "San Francisco"
+California << "Los Angeles"
+California << "Sacramento"
+California << "San Jose"
+p California
+California.delete_at(2)
+p California
+California.insert(2, "Mill Valley")
+p California
+California.shift
+p California
+if California.include?("Mill Valley")
+	puts "That is in California!"
+else
+	puts "Location not found.  Please try again later or incorporate that city/town."
+end
+
+
+Illinois = ["Chicago", "Springfield", "Naperville (Neighborville with a 'P'"]
+p Illinois
+Calinois = []
+Calinois << California << Illinois
+p Calinois
