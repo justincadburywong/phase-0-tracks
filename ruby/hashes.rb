cb9368f57d5825cd1b472bcbd0cef384ba39e742
Justincadburywong <justincadburywong@gmail.com>
2016-04-02 15:20:20 -0700

    update with loop about changing anything

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,17 +5,45 @@ interior_designer = gets.chomp.to_i
 
 until interior_designer == 0
  #asks for a name and turns it into the hash:
-  puts "what's your client's full name?"
-  client_name = gets.chomp.to_s
+  puts "what's your name?"
+  client_name = gets.chomp
   client_name = {}
 
-#asks for any info from the client and about their preferences
-  puts "What do you want to add about your client"
+#ask loop for any info from the client and about their preferences
+  puts "What is your age?"
+    age = gets.chomp.to_i
+  puts "How many children do you have?"
+  	children = gets.chomp.to_i
+  puts "What type of decor theme do you like?"
+    decor = gets.chomp.to_s
 
+# client_data = {
+# 	client_name: client_name,
+# 	age: age,
+# 	children: children,
+# 	decor: decor,
+# }
+client_name["age"] = age
+client_name["children"] = children
+client_name["decor"] = decor
 
+#prints out the hash so the user can see it
+  p "#{client_name}"
+  
+#give them an opportunity to corrent anything
+puts "Do you need to change anything?"
+change = gets.chomp
 
-#checks to see if hash is fully popluated
-  p client_name
+if change == "yes"
+	puts "Let me know what needs to be changed:"
+	new_key = gets.chomp
+	puts "What is it changed to?"
+	new_value = gets.chomp
+	client_name[new_key.sym] = new_value
+end
+
+p "#{client_name}"
+  
  puts "Thanks for the info!  We've stored it neatly away!"
  interior_designer -= 1
 end
