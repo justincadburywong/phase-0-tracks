823c6ac4fafe5d296d408ad94be3006884ea6b06
Justincadburywong <justincadburywong@gmail.com>
2016-04-02 16:56:43 -0700

    initial commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,19 @@
+grocery_store = {
+	'people' => [
+		'employees',
+		'customers',
+	],
+	'groceries' => [
+		'water',
+		'canned goods',
+		'bread',
+		'produce',
+		'cheese',
+		'meat',
+		'beer',
+		'wine',
+		'bodycare',
+		'prepared foods',
+		'frozen foods'
+	]
+}
