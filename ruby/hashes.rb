09dbd200c75252f9a2844ee7fcfd94ba09b03c4a
Justincadburywong <justincadburywong@gmail.com>
2016-04-02 11:08:44 -0700

    update to puts a response into a hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,13 +4,18 @@ puts "Hello fabulous!  What details about your client will you be entering today
 interior_designer = gets.chomp.to_i
 
 until interior_designer == 0
- #do stuf
+ #asks for a name and turns it into the hash:
+  Puts "what's your full name?"
+  full_name = gets.chomp.to_s
+  full_name = {}
+
+#asks for any info from the client and about their preferences
+ 
 
 
 
-
-
-
+#checks to see if hash is fully popluated
+  p full_name
  puts "Thanks for the info!  We've stored it neatly away!"
  interior_designer -= 1
 end
