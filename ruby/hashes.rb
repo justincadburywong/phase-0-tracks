0f8672c9b44765fccb4441b8214247d72daef2d3
Justincadburywong <justincadburywong@gmail.com>
2016-04-02 11:10:30 -0700

    changed full_name to client_name

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,17 +5,17 @@ interior_designer = gets.chomp.to_i
 
 until interior_designer == 0
  #asks for a name and turns it into the hash:
-  Puts "what's your full name?"
-  full_name = gets.chomp.to_s
-  full_name = {}
+  puts "what's your client's full name?"
+  client_name = gets.chomp.to_s
+  client_name = {}
 
 #asks for any info from the client and about their preferences
- 
+  puts "What do you want to add about your client"
 
 
 
 #checks to see if hash is fully popluated
-  p full_name
+  p client_name
  puts "Thanks for the info!  We've stored it neatly away!"
  interior_designer -= 1
 end
