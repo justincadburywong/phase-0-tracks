11928b82cf170b2581c67eb76a8a37964e87ebf0
Justincadburywong <justincadburywong@gmail.com>
2016-04-02 10:58:49 -0700

    update condition loop for how many interior designers

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+
+#ask for how many interior designers will be entering details for their client:
+puts "Hello fabulous!  What details about your client will you be entering today?"
+interior_designer = gets.chomp.to_i
+
+until interior_designer == 0
+ #do stuf
+
+
+
+
+
+
+ puts "Thanks for the info!  We've stored it neatly away!"
+ interior_designer -= 1
+end
