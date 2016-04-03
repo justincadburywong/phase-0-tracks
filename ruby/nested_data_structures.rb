47958fed23a9ad3be9b84de8aa8e26ba8f55f550
Justincadburywong <justincadburywong@gmail.com>
2016-04-02 19:50:53 -0700

    update with three working queries

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,19 +1,54 @@
 grocery_store = {
-	'people' => [
-		'employees',
-		'customers',
+	people: [
+		"employees",
+		"customers"
 	],
-	'groceries' => [
-		'water',
-		'canned goods',
-		'bread',
-		'produce',
-		'cheese',
-		'meat',
-		'beer',
-		'wine',
-		'bodycare',
-		'prepared foods',
-		'frozen foods'
+	products: [
+		"produce",
+		"cheese",
+		"meat",	
+		"bodycare",
+		"prepared_foods",
+		"frozen_foods",
+		canned_goods: [
+			"soups",
+			"beans",
+			"pasta_sauce",
+			"vegetables"
+			],
+		bread: [
+			"white",
+			"wheat"
+			],
+		alcohol: [
+			"beer",
+			"wine",
+			"spirits"
+		],
+	],
+	departments: [
+		"grocery",
+		"dairy",
+		"frozen",
+		"bulk",
+		"produce",
+		"floral",
+		"beer_and_wine",
+		"cheese",
+		"bakery",
+		"meat",
+		"seafood",
+		"prepared_foods",
+		"customer_service",
+		whole_body: [
+			"bodycare",
+			"vitamins",
+			"housewares"
+		],
 	]
 }
+
+p grocery_store[:people][1]
+p grocery_store[:products][2]
+p grocery_store[:departments][1]
+p grocery_store[:departments][13][1]
