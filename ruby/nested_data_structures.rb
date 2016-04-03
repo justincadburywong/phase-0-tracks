8ff11ebf5126e79220079cb66ac43d9f0dc00e15
Justincadburywong <justincadburywong@gmail.com>
2016-04-02 20:22:04 -0700

    updated with hashes and arrays, symbols, and better readability

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,54 +1,55 @@
 grocery_store = {
-	people: [
-		"employees",
-		"customers"
-	],
-	products: [
-		"produce",
-		"cheese",
-		"meat",	
-		"bodycare",
-		"prepared_foods",
-		"frozen_foods",
-		canned_goods: [
-			"soups",
-			"beans",
-			"pasta_sauce",
-			"vegetables"
-			],
-		bread: [
-			"white",
-			"wheat"
-			],
-		alcohol: [
-			"beer",
-			"wine",
-			"spirits"
-		],
-	],
-	departments: [
-		"grocery",
-		"dairy",
-		"frozen",
-		"bulk",
-		"produce",
-		"floral",
-		"beer_and_wine",
-		"cheese",
-		"bakery",
-		"meat",
-		"seafood",
-		"prepared_foods",
-		"customer_service",
-		whole_body: [
-			"bodycare",
-			"vitamins",
-			"housewares"
-		],
-	]
+  people: [
+  	:employees,
+  	:customers
+  ],
+  products: {
+  	produce: "perishable",
+  	cheese: "perishable",
+  	meat: "perishable",	
+  	bodycare: "non-perishable",
+  	prepared_foods: "perishable",
+  	frozen_foods: "perishable",
+  	canned_goods: {
+  	  soups: "non-perishable",
+  	  beans: "non-perishable",
+  	  pasta_sauce: "non-perishable",
+  	  vegetables: "non-perishable"
+  	  },
+  	bread: {
+  	  white: "non-perishable",
+  	  wheat: "perishable"
+  	  },
+  	alcohol: {
+  	  beer: "perishable",
+  	  wine: "non-perishable",
+  	  spirits: "non-perishable"
+  	},
+  },
+  departments: [
+  	:grocery,
+  	:dairy,
+  	:frozen,
+  	:bulk,
+  	:produce,
+  	:floral,
+  	:beer_and_wine,
+  	:cheese,
+  	:bakery,
+  	:meat,
+  	:seafood,
+  	:prepared_foods,
+  	:customer_service,
+  	whole_body: [
+  	  :bodycare,
+  	  :vitamins,
+  	  :housewares
+  	],
+  ]
 }
 
 p grocery_store[:people][1]
-p grocery_store[:products][2]
+p grocery_store[:products][:produce]
+p grocery_store[:products][:canned_goods][:beans]
 p grocery_store[:departments][1]
-p grocery_store[:departments][13][1]
+p grocery_store[:departments][13]
