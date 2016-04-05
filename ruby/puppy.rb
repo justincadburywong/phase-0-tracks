df86bb3744063acd7acd98b73ee6e4c9ad32bee8
Justincadburywong <justincadburywong@gmail.com>
2016-04-05 11:21:16 -0700

    put actual work in puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,11 @@
+# species
+# # canus lupus familiaris
+
+# characteristics
+# # energy level: 10
+# # coordinated: false
+# # hungry: true
+# # obedient: false
+# # name: varies
+# # color: varies
+
