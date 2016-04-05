65a1d249c8c782766b0e0f7712ad004f0ab948ab
Justincadburywong <justincadburywong@gmail.com>
2016-04-05 13:55:08 -0700

    release 2 and 3 with beer class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,8 @@
 class Puppy
+  
+  def initialize
+    puts "Initializing new puppy instance..."    
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -24,6 +28,7 @@ class Puppy
 end
 
 fido = Puppy.new
+
 fido.fetch("rubber duck")
   
 fido.speak(4)  
@@ -33,3 +38,45 @@ fido.roll_over
 puts fido.dog_years(4)
 
 puts fido.hungry
+
+spot = Puppy.new
+
+class Beer
+  
+  def initialize
+    @beer_count = 0    
+  end
+
+  def taster_flight
+    @beer_count += 6
+  end
+  
+  def beer_me
+    @beer_count += 1    
+  end
+  
+  def last_call
+    if @beer_count >= 7
+      puts "Go home!"
+    end
+  end
+  
+end
+
+beer = Beer.new
+beer.taster_flight
+beer.last_call
+
+beers = []
+idx = 0
+while idx < 50 do
+  beers << Beer.new
+  idx += 1
+end
+p beers
+
+beers.each do |beer|
+  beer.taster_flight
+  beer.beer_me
+  beer.last_call
+end
