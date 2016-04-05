781ca3e7139254d64fb22535e1e89faa4c9145ee
Justincadburywong <justincadburywong@gmail.com>
2016-04-05 13:24:29 -0700

    inital commit with driver code for basic behaviors

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,10 +1,35 @@
-
 class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(i)
+    i.times {puts ("woof")}  
+  end
 
+  def roll_over
+    puts "roll over"
+  end
+  
+  def dog_years(x)
+    return x*7
+  end
+    
+  def hungry
+    return true 
+  end
+  
 end
 
+fido = Puppy.new
+fido.fetch("rubber duck")
+  
+fido.speak(4)  
+
+fido.roll_over
+
+puts fido.dog_years(4)
+
+puts fido.hungry
