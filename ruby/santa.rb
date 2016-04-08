3e625426f6039d9ade9c95eea3756add650ba458
Justincadburywong <justincadburywong@gmail.com>
2016-04-08 00:06:38 -0700

    initial commit

---
+++ ruby/santa.rb
@@ -0,0 +1,14 @@
+class Santa
+	def initialize
+		print "Initializing Santa instance ..."
+		def @speak
+			puts "Ho, ho, ho! Haaaappy holidays!"
+		end
+		def @eat_milk_and_cookies(cookie_type)
+			print "That was a good #{cookie_type}!" 
+		end
+
+	end
+
+
+chris = santa.new
