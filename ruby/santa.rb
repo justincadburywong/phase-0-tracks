eccdcdc85054fbd363402856107512dc52627d87
Justincadburywong <justincadburywong@gmail.com>
2016-04-08 00:14:20 -0700

    santa class added with driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,17 @@
 class Santa
 	def initialize
 		print "Initializing Santa instance ..."
-		def @speak
+		def speak
 			puts "Ho, ho, ho! Haaaappy holidays!"
 		end
-		def @eat_milk_and_cookies(cookie_type)
+		def eat_milk_and_cookies(cookie_type)
 			print "That was a good #{cookie_type}!" 
 		end
 
 	end
+end
 
 
-chris = santa.new
+chris = Santa.new
+p chris.speak
+p chris.eat_milk_and_cookies("snickerdoodle")
