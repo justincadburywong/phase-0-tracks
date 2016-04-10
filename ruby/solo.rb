3b24b98922a6448d3351a48162fb69bd2a6c2886
Justincadburywong <justincadburywong@gmail.com>
2016-04-09 23:25:43 -0700

    added more pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,7 @@
+# a class on a glass of beer!
+# my class will have simple attributes:  @drinkability(integer), @beer_level(integer), and @color(string)
+# drinkability increases the changes of it being finished, so i'll have a method that interacts with that somehow
+# i can have a method that checks the beer_level while it's being drunk, so give it a current state
+# and a method that at least talks about the color
+
+
