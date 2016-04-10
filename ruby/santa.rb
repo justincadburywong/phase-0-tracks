2e40b8bbee51f5d77955ba3452f89bf56a50a3e1
Justincadburywong <justincadburywong@gmail.com>
2016-04-09 21:22:36 -0700

    added loop to trigger random santa instances, release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,13 @@
 class Santa
-	def initialize
+	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	@age = 0
+
+	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
 		print "Initializing Santa instance ..."
+		puts "This santa identifies as a #{@gender} #{@ethnicity}."
+		puts
 	end
 
 	def speak
@@ -18,7 +21,24 @@ class Santa
 
 end
 
+# chris = Santa.new("male", "chinese")
+# chris.speak
+# chris.eat_milk_and_cookies("snickerdoodle")
 
-chris = Santa.new
-p chris.speak
-p chris.eat_milk_and_cookies("snickerdoodle")
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
+
+loop do
+	puts "press enter to generate a random santa.  type 'done' when finished."
+	response = gets.chomp
+	break if response == "done"
+	genders_length=example_genders.length
+	ethnicities_length=example_ethnicities.length
+	santas=Santa.new(example_genders[rand(genders_length)],example_ethnicities[rand(ethnicities_length)])
+end
+
