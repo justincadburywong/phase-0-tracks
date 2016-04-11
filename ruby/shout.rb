65f7494fe1a8a2b09cce5ad40ef2588ddbb9e19d
Justincadburywong <justincadburywong@gmail.com>
2016-04-10 21:28:34 -0700

    redefined shout module to make mixin version

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,19 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
 
-	def self.yelling_happily(words)
-		"I can't contain myself!!  " + words + "!!!" + "  :):):):)"
+# 	def self.yelling_happily(words)
+# 		"I can't contain myself!!  " + words + "!!!" + "  :):):):)"
+# 	end
+# end
+
+# puts Shout.yell_angrily("No way!!  I can't take this anymore!")
+# puts Shout.yelling_happily("I can't believe this worked")
+
+
+module Shout
+	def alert(name)
+		puts "Hold it right there #{name}!!"
 	end
 end
-
-puts Shout.yell_angrily("No way!!  I can't take this anymore!")
-puts Shout.yelling_happily("I can't believe this worked")
