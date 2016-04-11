04ccf55c50c213efeb3682849ffc7d291dcb47f0
Justincadburywong <justincadburywong@gmail.com>
2016-04-10 21:02:22 -0700

    with driver code

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,6 +1,6 @@
 module Flight
 	def take_off(altitude)
-		puts "Taking off and ascending until reaching #{altitude}..."
+		puts "Taking off and ascending until reaching #{altitude}ft..."
 	end
 end
 
@@ -12,3 +12,10 @@ end
 class Plane
 	include Flight
 end
+
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
