ac3cdb7ffb94bba5de8034cb5f291e68a4ffc019
Justincadburywong <justincadburywong@gmail.com>
2016-04-15 20:02:01 -0700

    updated pseudocode for JS function

--- js/explore.js
+++ js/explore.js
@@ -1,5 +1,7 @@
 // declare a (var)iable called  =reverse that takes a (str);
-// 
-// 
-// 
-// 
+//   then call the (str) to .reverse by calling what is in 'quotes' 
+//   to reverse("this will be reversed")
+// make sure to console.log(the variable =reverse(with a parameter!))
+// add lots of ())();););(;(;(;);)) 
+
+
