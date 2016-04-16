687b3e51291b607b2c276c41d98cfe8eb1f1ee57
Justincadburywong <justincadburywong@gmail.com>
2016-04-15 20:06:56 -0700

    added function with driver code, FAIL

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,12 @@
-// declare a (var)iable called  =reverse that takes a (str);
+// declare a function called  =reverse that takes a (str);
 //   then call the (str) to .reverse by calling what is in 'quotes' 
 //   to reverse("this will be reversed")
 // make sure to console.log(the variable =reverse(with a parameter!))
 // add lots of ())();););(;(;(;);)) 
 
 
+function reverse(str) {
+	return reverse(str);
+}
+
+console.log(reverse("i want this to work out!"));
