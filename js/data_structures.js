e1174eb11f47ec6d7f56fabe1fa1ab724b1eec93
Justincadburywong <justincadburywong@gmail.com>
2016-04-15 21:33:21 -0700

    updated 'for' variable loop to iterate over arrays and populate a hash

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,6 +4,29 @@ var names = ["william", "justin", "ed", "tom"];
 colors.push("pink");
 names.push("fred");
 
+var coloredHorses = {}
+
 
 console.log(colors)
 console.log(names)
+
+
+
+// var blue = colors[0]
+// console.log(coloredHorses.colors[0] = names[0])
+// console.log(coloredHorses.colors = "names");
+// 
+
+// for (var i = 0; i < colors.count; i++) {
+//   console.log(number[i]);
+// }
+
+
+for(var i=0; i<names.length; i++){
+    var coloredHorses = {};
+    for(var j=0; j<names[i].length; j++){
+         coloredHorses[names[j]] = colors[j];  
+      }
+
+}
+console.log(coloredHorses);
