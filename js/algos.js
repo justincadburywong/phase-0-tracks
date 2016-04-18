00e1e2b94f994a76e9c7f851a052a280d8d0384c
Justincadburywong <justincadburywong@gmail.com>
2016-04-18 08:49:11 -0700

    pseudocode for release 1

---
+++ js/algos.js
@@ -0,0 +1,22 @@
+// In algos.js, write a function that takes an array of words or 
+// phrases and returns the longest word in the array. So if we gave 
+// your function the array of ["long phrase","longest phrase","longer 
+// phrase"], it would return "longest phrase". This is a logical 
+// reasoning challenge, so try to reason through this based on what 
+// you already know, rather than Googling the fanciest solution or 
+// looking up built-in sorting functions. Note that "some string".length 
+// will do what you expect in JS.
+
+/* 
+set an empty variable to hold the longest word in the array
+iterate over the array
+get the length of each index of the array
+put the first word in the array into the empty variable
+if the second word length is longer than the first, replace that in the empty variable
+continue iterating over the array until the longest word is in the empty variable
+print the empty variable
+*/
+
+
+
+// Add driver code that tests your function on a few arrays.
