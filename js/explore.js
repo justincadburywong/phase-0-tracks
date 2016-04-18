2ba71d57820d1c0b8c53b281ba46c32098d0ff6b
Justincadburywong <justincadburywong@gmail.com>
2016-04-18 00:01:52 -0700

    updated pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,17 +1,23 @@
 // declare a function called  =reverse that takes a (str);
-//   then call the (str) to .reverse by calling what is in 'quotes' 
-//   to reverse("this will be reversed")
+//   then call the (str) to split each letter in place, including spaces
+//	 call .reverse on the whole string
+//   join everything back together into one string, including spaces
 // make sure to console.log(the variable =reverse(with a parameter!))
 // add lots of ())();););(;(;(;);)) 
 
 
 function reverse(str) {
-	return str.split('').reverse().join("");
+	return str.split('').reverse().join('');
 }
 
 newString = reverse("i want this to work out!");
 
-for(var i=0; i==1;) {
+// for(var i=0; i==1;) {
+// 	console.log(newString);
+// }
+// i==1
+var i = 0
+if (i == 1) {
 	console.log(newString);
 }
-i==1
+i = 1
