6c50a11ae2dd866c3e948c5f4f1fe7e5cf74b144
Justincadburywong <justincadburywong@gmail.com>
2016-04-19 05:37:48 -0700

    changed compare functionto use key-value pair comparason

--- js/algos.js
+++ js/algos.js
@@ -69,11 +69,15 @@ it should evaluate to false...  once more.
 */
 
 function compare(object1, object2) {
-	for (var value in object1) {
-		 if (object1[value] === object2[value]){
-			return true
-		} else {
-			return false	
+	for (var key in object1) {
+		 if (object1[key] === object2[key]){
+		 	for (var value in object1) {
+		 		if (object1[value] === object2[value]){
+					return true
+				} else {
+					return false	
+				}
+			}
 		}
 	}
 }
