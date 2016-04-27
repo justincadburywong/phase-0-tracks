0fa9dc2801c195badb420a6e6780cd4602980770
Justincadburywong <justincadburywong@gmail.com>
2016-04-27 15:40:24 -0700

    added function to add element

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,14 +1,15 @@
 console.log("The script is running!");
 
-// function addElement() {
-// 	var newElement = document.createElement("div");
-// 	document.body.insertBefore(footer);
+function addElement() {
+	var newElement = document.createElement("div");
+	var newContent = document.createTextNode("Hi there and greetings!"); 
+  newElement.appendChild(newContent);
 
-// 	var ourFooter = document.getElementsByTagName("FOOTER");
-// 	document.body.insertBefore(newElement, ourFooter)
-// }
+	var ourDiv = document.getElementById("second_div");
+	document.body.insertBefore(newElement, ourDiv);
+}
 
-// addElement()
+// addElement();
 
 
 function changeStyle() {
@@ -16,5 +17,5 @@ function changeStyle() {
 	changeColor.style.backgroundColor = "black";
 }
 
-changeStyle();
 
+// changeStyle();
