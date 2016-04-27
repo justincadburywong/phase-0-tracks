8c207166c5bf2d425ca026b15aa4ac68d69435bc
Justincadburywong <justincadburywong@gmail.com>
2016-04-27 15:33:53 -0700

    finally got a function to work in the script

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,20 @@
 console.log("The script is running!");
+
+// function addElement() {
+// 	var newElement = document.createElement("div");
+// 	document.body.insertBefore(footer);
+
+// 	var ourFooter = document.getElementsByTagName("FOOTER");
+// 	document.body.insertBefore(newElement, ourFooter)
+// }
+
+// addElement()
+
+
+function changeStyle() {
+	var changeColor = document.getElementById("Frustrated_first_header");
+	changeColor.style.backgroundColor = "black";
+}
+
+changeStyle();
+
